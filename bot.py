6a45be1f640d951370fd8077d1769faddf324175
Vusalldidaa <95521178+Vusalldidaa@users.noreply.github.com>
2022-01-10 19:44:56 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,9 +10,9 @@ logging.basicConfig(
 )
 LOGGER = logging.getLogger(__name__)
 
-api_id = int(os.environ.get("API ID"))
-api_hash = os.environ.get("API HASH")
-bot_token = os.environ.get("BOT TOKEN")
+api_id = int(os.environ.get("APP_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("BOT_TOKEN")
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 
 anlik_calisan = []
